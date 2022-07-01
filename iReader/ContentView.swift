a24652bbdadde724daab6f4e59b5141621741f68
dekapd99 <dekaprimatio@gmail.com>
2022-07-01 22:15:26 +0700

    Initial Commit

---
+++ iReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iReader
+//
+//  Created by Deka Primatio on 01/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iReader/iReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iReaderApp.swift
+//  iReader
+//
+//  Created by Deka Primatio on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
